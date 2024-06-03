7b592153249c71119d6d009c050e7fa3235e9dcf
SifiFox <dima.antonovich.1999@gmail.com>
2024-06-03 23:02:45 +0300

    feat: complete task3

--- lection_2/homework.js
+++ lection_2/homework.js
@@ -6,11 +6,11 @@ for (let i = 0; i < 5; i++) {
         console.log(i);
     };
 }
-result[0](); //5
-result[1](); //5
-result[2](); //5
-result[3](); //5
-result[4](); //5
+// result[0](); //5
+// result[1](); //5
+// result[2](); //5
+// result[3](); //5
+// result[4](); //5
 
 //////////////////////////////////////////////////
 
@@ -31,25 +31,32 @@ function getGroup() {
 
 let group = getGroup();
 
-group[0](); // 10 как исправить на 0
-group[5](); // 10                  5
+// group[0](); // 10 как исправить на 0
+// group[5](); // 10                  5
 
 //////////////////////////////////////////////////
 
 // Напишите функцию multiply, должна принимать произвольное количество аргументов и возвращать их произведение.
 
-// const result1 = multiply(2)(3)(4);
+
+function multiply(arg){
+    return (arg2) => {
+        return arg2 ? multiply(arg * arg2) : arg
+    };
+}
+
+// const result1 = multiply(2)(3)(4)();
 // console.log(result1); // Вывод: 24
-// const result2 = multiply(2)(3)(4)(5);
+// const result2 = multiply(2)(3)(4)(5)();
 // console.log(result2); // Вывод: 120
 
 // const result1 = multiply(2)(3)(4);
-//
-// // Пример использования:
-// const result1 = multiply(2)(4);
+
+// Пример использования:
+// const result1 = multiply(2)(4)();
 // console.log(result1); // Вывод: 8
-//
-// const result2 = multiply(5)(2)(3);
+
+// const result2 = multiply(5)(2)(3)();
 // console.log(result2); // Вывод: 30
 
 /////////////////////////
