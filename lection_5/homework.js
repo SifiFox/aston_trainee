226bf05c8c1ea9898a2855fecd631d8d8e62952e
SifiFox <dima.antonovich.1999@gmail.com>
2024-06-14 22:51:11 +0300

    feat: complete homework5

--- lection_5/homework.js
+++ lection_5/homework.js
@@ -1,33 +1,32 @@
 //1.
-// const a = {b: 1},
-//     c = Object.create(a);
-//
-// console.log(c.b); // ?
-// delete c.b;
-// console.log(c.b); // ?
+const a = {b: 1},
+    c = Object.create(a);
+
+// console.log(c.b); // 1 - Создан объект 'c' на основе объекта 'а', по цепочке находит свойство в прототипе
+// // delete c.b;
+// console.log(c.b); // 1 - Удалили свойсто из 'c', по цепочке прототипов вызовется из 'a'.
 // delete a.b;
-// console.log(c.b); // ?
+// console.log(c.b); // undefined - поскольку удалили свойство у прототипа, на основании которого объявлено 'c'
 // a.z = 2;
-// console.log(c.z); // ?
+// console.log(c.z); // 2 - свойство задано у 'a', которое является прототипом 'c'
 // c.z = 3;
-// console.log(a.z); // ?
+// console.log(a.z); // 2 - z изменили у объекта 'c', но не у прототипа
 
 // 2.
 
-// const promise = new Promise(() => {
-// })
-// promise.prototype === Promise.__proto__ // ?
-//
-// const obj = {}
-// obj.__proto__ === Object.prototype // ?
-//
-// new Array([]).__proto__ === Array.prototype // ?
-//
+const promise = new Promise(() => {
+})
+// console.log(promise.prototype === Promise.__proto__) // false;  promise.__proto__ === Promise.prototype -> true
+
+const obj = {}
+
+// console.log(obj.__proto__ === Object.prototype) // true
+// console.log(new Array([]).__proto__ === Array.prototype) // true
+
 // function Fn1 () {}
 // function Fn2 () {}
-// Fn1.constructor === Fn2.constructor // ?
-//
-// Fn1.prototype === Fn2.prototype // ?
+// console.log(Fn1.constructor === Fn2.constructor) // true, один и тот же конструктор - Function
+// console.log(Fn1.prototype === Fn2.prototype )// false, поскольку объект prototype создается для каждой функции свой
 //3.
 
 // У вас есть два конструктора, Animal и Bird.
@@ -49,4 +48,82 @@
 // bird.speak();   // "Some generic sound"
 // bird.fly();     // "Flying high!"
 
+////////////////////////////////////
+///////////  на классах ////////////
+////////////////////////////////////
 
+// class Animal {
+//     constructor(name) {
+//         this.name = name
+//     }
+//     speak() {
+//         console.log('Some generic sound')
+//     }
+// }
+// class Bird extends Animal{
+//     constructor() {
+//         super();
+//     }
+//     fly(){
+//         console.log('Flying high!')
+//     }
+// }
+//
+//
+// const animal = new Animal("Дженни")
+// const bird = new Bird("Воробей")
+//
+// animal.speak()
+// bird.speak()
+// bird.fly()
+
+
+///////////////////////////////////////////
+///////// Функция конструктор /////////////
+///////// через call /////////
+///////////////////////////////////////////
+// function Animal(name){
+//     this.name = name
+//     this.speak = function(){
+//         console.log('Some generic speak!')
+//     }
+// }
+//
+// function Bird(name){
+//     Animal.call(this, name)
+//     this.fly = function () {
+//         console.log('Flying high')
+//     }
+// }
+//
+// const animal = new Animal("Дженни")
+// const bird = new Bird("Воробей")
+//
+// animal.speak()
+// bird.speak()
+// bird.fly()
+
+
+///////////////////////////////////////
+///////// Функция конструктор /////////
+///////// прототипы  //////////////////
+///////////////////////////////////////
+// function Animal(name) {
+//     this.name = name
+//     this.speak = function(){
+//         console.log('Some generic speak!')
+//     }
+// }
+// function Bird(name) {
+//     this.name = name
+//     this.fly = function(){
+//         console.log('Flying high')
+//     }
+// }
+// const animal = new Animal('Дженни')
+// Bird.prototype = animal
+// const bird = new Bird('Воробей')
+//
+// animal.speak()
+// bird.speak()
+// bird.fly()
